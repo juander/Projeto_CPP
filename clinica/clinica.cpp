5b4e6485d3375cb7afecfad835636beff7b5f8fe
levi <98407657+Levirbss@users.noreply.github.com>
2025-01-10 12:53:52 -0300

    Delete clinica directory

--- clinica/clinica.cpp
+++
@@ -1,9 +0,0 @@
-#include "clinica.h"
-using namespace std;
-
-Paciente::Paciente(string n, int i, string d) : nome(n), idade(i), diagnostico(d) {}
-
-string Paciente::getInfo() const {
-    return "Nome: " + nome + ", Idade: " + to_string(idade) + ", Diagnóstico: " + diagnostico;
-}
-
