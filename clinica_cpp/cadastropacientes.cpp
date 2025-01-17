05fec1398a62743469e1e65f914220fada073302
Levirbss <levi.serrano1204@gmail.com>
2025-01-17 14:14:36 -0300

    adição de comentarios no cadastropaciente.cpp

--- clinica_cpp/cadastropacientes.cpp
+++ clinica_cpp/cadastropacientes.cpp
@@ -2,12 +2,19 @@
 #include "ui_cadastropacientes.h"
 #include <QMessageBox>
 
+// DEFINIÇÃO DO CONSTRUTOR DA CLASSE CADASTROPACIENTE
+
 cadastroPacientes::cadastroPacientes(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::cadastroPacientes)
 {
     ui->setupUi(this);
 }
+
+// FIM
+
+
+// DEFINIÇÃO DO DESTRUTOR DA CLASSE CADASRTROPACIENTE
 
 cadastroPacientes::~cadastroPacientes()
 {
@@ -16,6 +23,7 @@ cadastroPacientes::~cadastroPacientes()
 
 void cadastroPacientes::on_btnCadastrar_clicked()
 {
+    // Recupera os dados dos campos da interface do usuário                                                         // OBTÉM OS DADOS DOS CAMPOS DA INTERFACE
     QString nom = ui->txtnome->text();
     QString cpf = ui->txtCpf->text();
     QDate dataNascimento = ui->txtData->date();
@@ -24,16 +32,18 @@ void cadastroPacientes::on_btnCadastrar_clicked()
     QString emai = ui->txtemail->text();
     QString conven = ui->txtconvenio->text();
 
+    // Cria um objeto Paciente com os dados recuperados
     Paciente paciente(nom, cpf, contat, emai, dataNascimento, conven, diagn);
 
+    // Conecta o sinal pacienteCadastrado ao slot correspondente
     connect(&paciente, &Paciente::pacienteCadastrado, this, &cadastroPacientes::pacienteCadastrado);
 
-    // Salvar no banco
+    // Salva o objeto Paciente no banco de dados
     if (paciente.salvarNoBanco()) {
         QMessageBox::information(this, "", "Cadastro realizado");
         this->close();
     } else {
-        QMessageBox::information(this, "ERRO", "Erro ao cadastrar paciente");
+        QMessageBox::information(this, "ERRO", "Erro ao cadastrar paciente");                                      // Mensagem de erro
     }
 }
 
