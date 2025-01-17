7e5d5e98415b2e190ea66c987695a452654be3b3
Levirbss <levi.serrano1204@gmail.com>
2025-01-17 14:16:52 -0300

    adição de comentarios no cadastropaciente.cpp

--- clinica_cpp/cadastropacientes.cpp
+++ clinica_cpp/cadastropacientes.cpp
@@ -14,40 +14,53 @@ cadastroPacientes::cadastroPacientes(QWidget *parent)
 // FIM
 
 
-// DEFINIÇÃO DO DESTRUTOR DA CLASSE CADASRTROPACIENTE
+// DEFINIÇÃO DO DESTRUTOR DA CLASSE CADASTROPACIENTE
 
 cadastroPacientes::~cadastroPacientes()
 {
     delete ui;
 }
 
+// FIM
+
+
+
+// DEFINIÇÃO DO METODO CADASTRAR PACIENCE SELECIONADO
+
 void cadastroPacientes::on_btnCadastrar_clicked()
 {
     // Recupera os dados dos campos da interface do usuário                                                         // OBTÉM OS DADOS DOS CAMPOS DA INTERFACE
-    QString nom = ui->txtnome->text();
+    QString nom = ui->txtnome->text();                                                                                   
     QString cpf = ui->txtCpf->text();
     QDate dataNascimento = ui->txtData->date();
     QString diagn = ui->txtDiagn->text();
     QString contat = ui->txtcontato->text();
     QString emai = ui->txtemail->text();
-    QString conven = ui->txtconvenio->text();
+    QString conven = ui->txtconvenio->text(); 
 
-    // Cria um objeto Paciente com os dados recuperados
+    // Cria um objeto Paciente com os dados recuperados                                                            // CRIA UM OBJETO PACIENTE COM OS DADOS RECUPERADOS
     Paciente paciente(nom, cpf, contat, emai, dataNascimento, conven, diagn);
 
-    // Conecta o sinal pacienteCadastrado ao slot correspondente
+    // Conecta o sinal pacienteCadastrado ao slot correspondente                                                   // CONECTA O SINAL PACIENTE CADASTRADO AO SLOT CORRESPONDENTE
     connect(&paciente, &Paciente::pacienteCadastrado, this, &cadastroPacientes::pacienteCadastrado);
 
-    // Salva o objeto Paciente no banco de dados
+    // Salva o objeto Paciente no banco de dados                                                                   // SALVA O OBJETO PACIENTE NO BANCO DE DADOS
     if (paciente.salvarNoBanco()) {
         QMessageBox::information(this, "", "Cadastro realizado");
         this->close();
     } else {
-        QMessageBox::information(this, "ERRO", "Erro ao cadastrar paciente");                                      // Mensagem de erro
+        QMessageBox::information(this, "ERRO", "Erro ao cadastrar paciente");
     }
 }
+
+// FIM
+
+
+// DEFINIÇÃO DO METODO CANCELAR CADASTRO
 
 void cadastroPacientes::on_btnCancelar_clicked()
 {
     this->close();
 }
+
+// FIM
